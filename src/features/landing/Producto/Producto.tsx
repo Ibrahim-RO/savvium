f7664d6c876fa82913ee54413ac60a6e054451cb
Brisa Cuatepitzi <brisacuate12928@gmail.com>
2026-04-10 17:38:07 -0600

    feat: implement landing page structure and remove tailwind configuration file

--- src/features/landing/Producto/Producto.tsx
+++ src/features/landing/Producto/Producto.tsx
@@ -1,9 +1,11 @@
 import { ModulosGrid } from "./components/moduloGrid"
+import { ProblemasSection } from "./components/moduloComponents"
 
 export default function Producto() {
   return (
     <>
       <ModulosGrid />
+      <ProblemasSection />
     </>
   );
 }
