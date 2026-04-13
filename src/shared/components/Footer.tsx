f2ffe4a24d6ab5ad22da31cc892d025c8bc7cbed
ibrahim <ibra.rodriguez.olaya@gmail.com>
2026-04-13 09:25:37 -0600

    feat: enhance footer layout and add social media links

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -4,11 +4,18 @@ import Image from "next/image"
 import { navigation } from "../data/navigation";
 import Link from "next/link";
 
+const socialMedias = [
+    { name: "Facebook", ulr: "#" },
+    { name: "Instagram", ulr: "#" },
+    { name: "LinkedIn", ulr: "#" },
+    { name: "Whatsapp", ulr: "#" },
+]
+
 export default function Footer() {
     return (
         <footer className="bg-[#101829] text-white">
-            <div className="max-w-7xl mx-auto py-6 flex justify-between items-center">
-                <div>
+            <div className="max-w-7xl mx-auto py-6 grid grid-cols-1 lg:grid-cols-3 gap-8">
+                <div className="space-y-3">
                     <Image
                         src={"/savviumLogo.png"}
                         alt="Logo Savvium"
@@ -16,29 +23,42 @@ export default function Footer() {
                         height={150}
                         className="w-40"
                     />
-                    <p>El ERP inteligente para empresas en crecimiento</p>
+                    <p className="text-gray-300">El ERP inteligente para empresas en crecimiento</p>
+                    <div className="flex gap-3">
+                        <a 
+                            href="#"
+                            className="bg-white/10 p-2 rounded-full hover:bg-white/15 hover:transition-transform hover:scale-110 hover:duration-300"
+                        >
+                            Face
+                        </a>
+                    </div>
+                    {/* social medias : insta, tiktok, linkedin, whatsapp */}
                 </div>
                 <nav className="flex flex-col gap-2 justify-center items-center">
-                    <p>Enlaces</p>
+                    <p className="font-bold text-lg">Enlaces</p>
                     {navigation.map(item => {
                         return (
                             <Link
                                 key={item.name}
                                 href={item.url}
-                                className=""
+                                className="text-gray-300"
                             >
                                 {item.name}
                             </Link>
                         )
                     })}
                 </nav>
-                <div>
-                    <button
-                        className="bg-blue-600 hover:bg-blue-700 transition-colors px-6 py-2 rounded-lg text-white cursor-pointer font-semibold"
-                    >
-                        Agenda tu demo
-                    </button>
+                <div className="place-items-end">
+                    <div className="space-y-3 ">
+                        <p className="text-lg font-bold">¿Listo para empezar?</p>
+                        <button
+                            className="bg-blue-600 hover:bg-blue-700 transition-colors px-6 py-2 rounded-lg text-white cursor-pointer font-semibold"
+                        >
+                            Agenda tu demo
+                        </button>
+                    </div>
                 </div>
+
             </div>
             <hr />
             <p>&copy; {new Date().getFullYear()} Savvium. Todos los derechos reservados.</p>
