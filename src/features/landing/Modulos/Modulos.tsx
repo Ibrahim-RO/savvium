a62321219d1cf35899f8526a26d56cbbdfaf4647
Brisa Cuatepitzi <brisacuate12928@gmail.com>
2026-04-15 11:18:38 -0600

    feat: implement Modulos landing section with interactive feature cards

--- src/features/landing/Modulos/Modulos.tsx
+++ src/features/landing/Modulos/Modulos.tsx
@@ -1,8 +1,10 @@
 import { Section_2 } from "./components/Section_2";
+import { ModNeeds } from "./components/section1";
 
 export default function Modulos() { 
   return (
     <div>
+      <ModNeeds />
       <Section_2 />
     </div>
   );
